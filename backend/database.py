7abfe93d6da2898c25eedae77174a62f60b63d22
nariming <parknarim@ewha.ac.kr>
2026-05-29 12:09:09 +0900

    fix:database.py

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:DaneDVBkeuAFOVTZzXEIjSBkTAIMSvaV@zephyr.proxy.rlwy.net:49651/railway")
 print(f"DATABASE_URL: {DATABASE_URL}")
 
 engine = create_engine(DATABASE_URL)
